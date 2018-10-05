c0d7b4e349cc87f02ed4d621cd48c3ef0aa0ff20
NecoNeo <neo2314@qq.com>
2018-10-05 19:24:38 +0800

    fix

---
+++ src/android/Agora.java
@@ -0,0 +1,26 @@
+package com.chuwa.cordova.agora;
+
+// import org.apache.cordova.CordovaInterface;
+
+// import android.content.Intent;
+// import android.net.Uri;
+// import android.content.SharedPreferences;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+// import org.json.JSONObject;
+
+
+public class Agora extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        // if (TAG.equals(action)) {
+        // }
+        return false;
+    }
+
+}
