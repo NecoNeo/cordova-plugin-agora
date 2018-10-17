e60a2422c4819ae4659e740db54bbc4f7c2ed987
NecoNeo <neo2314@qq.com>
2018-10-17 17:39:43 +0800

    stuck in bugs...

--- src/android/ChatRoomActivity.java
+++ src/android/ChatRoomActivity.java
@@ -3,8 +3,21 @@ package com.chuwa.cordova.agora;
 import android.app.Activity;
 //import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.agora.rtc.Constants;
+import io.agora.rtc.IRtcEngineEventHandler;
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
 
 public class ChatRoomActivity extends Activity {
+
+    private static final String LOG_TAG = ChatRoomActivity.class.getSimpleName();
+    private RtcEngine mRtcEngine;
+
+    private final IRtcEngineEventHandler mRtcEventHandler = new IRtcEngineEventHandler() {
+        //
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +26,23 @@ public class ChatRoomActivity extends Activity {
         FakeR fakeR = new FakeR(this);
         // setContentView(R.layout.activity_chat_room);
         setContentView(fakeR.getId("layout", "activity_chat_room"));
+
+        // demo
+        initializeAgoraEngine();
     }
 
     public void dummy() {
         // do nothing
+    }
+
+    // Tutorial Step 1
+    private void initializeAgoraEngine() {
+        try {
+            mRtcEngine = RtcEngine.create(getBaseContext(), "fake_id", mRtcEventHandler);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, Log.getStackTraceString(e));
+
+            throw new RuntimeException("NEED TO check rtc sdk init fatal error\n" + Log.getStackTraceString(e));
+        }
     }
 }
